ea85559dab8ed963c27ef2bcf6aa5342aa604186
Maxim Matyuschenko <matyuschenko@gmail.com>
2016-12-28 16:59:08 +0300

    added scss support to gulp; fixed browserSync.reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create(),
     buildFolder = './build',
     sourceFolder = './source';
@@ -8,25 +10,35 @@ gulp.task('pug', function () {
     return gulp.src(sourceFolder + '/template/pages/*.pug')
         .pipe(pug())
         .pipe(gulp.dest(buildFolder))
-})
+});
+
+gulp.task('sass', function () {
+    return gulp.src(sourceFolder + '/style/app.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({ browsers: ['last 3 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1'] }))
+        .pipe(gulp.dest(buildFolder + '/css'))
+});
 
 gulp.task('serve', function () {
     browserSync.init({
         server: buildFolder
     });
 
-    browserSync.watch(buildFolder + '/*.*').on('change', browserSync.reload);
-})
+    browserSync.watch(buildFolder + '/**/*.*', browserSync.reload);
+    // return gulp.watch(buildFolder + '/**/*.*').on('change', browserSync.reload);
+    // browserSync.watch(buildFolder + '/*.*').on('change', browserSync.reload);
+});
 
 gulp.task('watch', function () {
     gulp.watch(sourceFolder + '/template/**/*.pug', gulp.series('pug'));
-})
+    gulp.watch(sourceFolder + '/style/**/*.scss', gulp.series('sass'));
+});
 
 gulp.task('default', gulp.series(
     'pug',
+    'sass',
     gulp.parallel(
         'watch',
-        'serve',
-        'reload'
+        'serve'
     )
-))
+));

0f063a81a0e3a7c2471889803a8fca69ffd9a0c3
Maxim Matyuschenko <matyuschenko@gmail.com>
2017-02-18 23:54:34 +0300

    added data and labels from file + basic style

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,58 @@
+var lang = 'eng';
+
 $(window).on('load', function () {
+    $.getJSON("/data/locale.json", function (locale) {
+        addLabels(locale);
+    });
+
     $.getJSON("/data/sources.json", function (sources) {
         showSources(sources);
-    })
-})
+    });
+});
 
 function showSources(sources) {
     var container = $('.sources');
+
     $(sources).each(function(i, s) {
-        var source = $('div').html(s['description_eng']);
+        var source = $('<div>').addClass('source'),
+            countries,
+            spheres,
+            examples,
+            tags;
+
+        $('<h4>').addClass('source__header').html(s['name_' + lang]).appendTo(source);
+        $('<a>').addClass('source__link').attr('href', s['url']).html(s['url']).appendTo(source);
+        $('<p>').addClass('source__description').html(s['description_' + lang]).appendTo(source);
+        countries = $('<ul>').addClass('source__countries').appendTo(source);
+        $('<span>').addClass('source__period').html(s['period']).appendTo(source);
+        $('<span>').addClass('source__machine-readable_' + s['machine_readability']).appendTo(source);
+        spheres = $('<ul>').addClass('source__spheres').appendTo(source);
+        examples = $('<ul>').addClass('source__examples').appendTo(source);
+        tags = $('<ul>').addClass('source__tags').appendTo(source);
+
+        $(s['countries']).each(function (i, c) {
+            $('<li>').addClass('source__country').html(c).appendTo(countries);
+        });
+
+        $(s['spheres']).each(function (i, c) {
+            $('<li>').addClass('source__sphere').html(c).appendTo(spheres);
+        });
+
+        $(s['examples' + lang]).each(function (i, c) {
+            $('<li>').addClass('source__example').html(c).appendTo(examples);
+        });
+
+        $(s['tags']).each(function (i, c) {
+            $('<li>').addClass('source__tag').html(c).appendTo(tags);
+        });
+
+
         container.append(source);
     });
+}
+
+function addLabels(locale) {
+    for (var selector in locale[lang]) {
+        $(selector).html(locale[lang][selector]);
+    }
 }

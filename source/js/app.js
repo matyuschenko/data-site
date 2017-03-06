514477a66d3c24ce85d974275baba9dd2cd93253
Maxim Matyuschenko <matyuschenko@gmail.com>
2017-03-07 00:12:44 +0300

    extended localization and added language switch with url param

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,9 @@
-var lang = 'en';
+var lang = 'ru';
 var locale;
 
 $(window).on('load', function () {
+    lang = QueryString.lang || 'en';
+
     $.getJSON("/data/locale.json", function (data) {
         locale = data;
         addLabels(locale);
@@ -21,6 +23,8 @@ function addLabels(locale) {
     for (var selector in locale[lang]['labels']) {
         $(selector).html(locale[lang]['labels'][selector]);
     }
+    document.title = locale[lang]['labels']['.logo__text'];
+    $('.sources__search').attr('placeholder', locale[lang]['search'])
 }
 
 function showSources(sources) {
@@ -103,3 +107,26 @@ function searchSources(event, sources) {
         showSources(filteredSources);
     }
 }
+
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}();

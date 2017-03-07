eaa34c36290d160085b1a49f72975e4e8c6c2ae7
Maxim Matyuschenko <matyuschenko@gmail.com>
2017-03-08 00:37:56 +0300

    added nodemailer - now we can send messages

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var mailConfig = require('./mail_config');
+
 var port = 3000;
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport({
+    service: mailConfig.service,
+    auth: {
+        user: mailConfig.user,
+        pass: mailConfig.pass
+    }
+});
+
+// setup email data with unicode symbols
+var mailOptions = {
+    from: mailConfig.from, // sender address
+    to: mailConfig.to, // list of receivers
+    subject: mailConfig.subject,
+    text: mailConfig.test
+}
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageIg, info.response);
+});
 
 app.use(express.static('build'));
 app.listen(port);
